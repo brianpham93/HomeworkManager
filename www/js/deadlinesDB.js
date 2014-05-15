b578a8df6c4e8300bbbd5cd73231a87b2cb0b584
thangpqfuhl <thangpq@outlook.com.vn>
2014-05-16 00:22:17 +0700

    Revert 2918288..1a92c70
    This rolls back to commit 2918288369a90fa261cc79ebe3b262cd307e463f.

--- www/js/deadlinesDB.js
+++ www/js/deadlinesDB.js
@@ -43,11 +43,11 @@ function getAllDeadlines_success(tx, results){
 			$('#allList').append('<li id = "'+allDeadline.duedate+' '+allDeadline.duetime+'"><a href="#DeadlineDetail" id = "'+
 				allDeadline.id+'" data-transition = "slide">'+ allDeadline.class +'<br>'+ allDeadline.duedate+'  '+ 
 				allDeadline.duetime+'<br>'+ allDeadline.description +'</a></li>');
-				alert(allDeadline.id);
+				//alert(allDeadline.id);
 				window.plugin.notification.local.add({
 					id : parseInt(allDeadline.id), 
-				    message: 'Dont forget to complete: '+allDeadline.description+'',
-				    date: notiDate
+				    message: 'Dont forget to complete: '+allDeadline.description+''
+				    //date: notiDate
 				});
 		}
 	}
@@ -825,7 +825,6 @@ function insertDeadlineToDB(dbId,dbDescription,dbClass,dbDueDate, dbDueTime, dbT
 function deleteDeadline(){
 	db.transaction(function(tx){
 		tx.executeSql("DELETE FROM deadlines WHERE id = ? ",[id], deleteSuccessCB, errorCB);
-		
 	});
 }
 // CALL BACK //
@@ -846,7 +845,6 @@ function insertSuccessCB(){
 
 function deleteSuccessCB(tx){
 	//alert("Deleted successfully");
-	//window.plugin.notification.local.cancel(id);
 	window.location.hash ="#deadlineList";
 }
 // External func
